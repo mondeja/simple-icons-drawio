c6ad452717744cafcf64018802c1e865d12b5015
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2022-11-29 02:12:16 +0100

    Simplify code

--- build.js
+++ build.js
@@ -43,9 +43,7 @@ const mxGraph = (svg) => {
 const encodeMxGraph = (xml) => encodeURIComponent(xml).replace('*', '%2A');
 
 const buildLibrary = () => {
-  const library = [];
-  for (const si in icons) {
-    const icon = icons[si];
+  return Object.values(icons).map((icon) => {
     const styledSvg =
       `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">`
       + `<style>*{fill:#${simplifyHexIfPossible(icon.hex)}}</style>`
@@ -53,14 +51,13 @@ const buildLibrary = () => {
       + `</svg>`
 
     const buffer = zlib.deflateSync(encodeMxGraph(mxGraph(styledSvg)));
-    library.push({
+    return {
       xml: buffer.slice(2, buffer.length - 4).toString("base64"),
       h: 144,
       w: 144,
       title: extractHtmlEncodedIconTitle(icon.svg),
-    });
-  }
-  return library;
+    }
+  });
 }
 
 fs.writeFileSync(
